f8d9d189342ff95d7479b99a2b9723f2e8287ffd
valentine39 <valentine.iat@hotmail.fr>
2026-04-16 17:15:06 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Backend OK"}
+
+@app.get("/api/test")
+def test():
+    return {"message": "ça marche"}
